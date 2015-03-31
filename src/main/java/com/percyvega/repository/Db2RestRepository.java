c4671652e2692cbd55a775e3f135374ac9396c43
Percy Vega <pevega@tracfone.com>
2015-03-31 11:14:42 -0400

    Small improvements

--- src/main/java/com/percyvega/repository/Db2RestRepository.java
+++ src/main/java/com/percyvega/repository/Db2RestRepository.java
@@ -3,16 +3,14 @@ package com.percyvega.repository;
 import com.percyvega.model.IntergateTransaction;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 
-public interface Db2RestRepository extends Repository<IntergateTransaction, Integer> {
+public interface Db2RestRepository extends PagingAndSortingRepository<IntergateTransaction, Long> {
 
     @Query(value = "select * from (select * FROM SA.INQ_RESULT where STATUS = :status and CARRIER_NAME = :carrierName order by CREATION_DATE desc) where rownum <= :count", nativeQuery = true)
-    public Collection<IntergateTransaction> find(@Param("status") String status, @Param("carrierName") String carrierName, @Param("count") int count) throws DataAccessException;
-
-    void save(IntergateTransaction intergateTransaction) throws DataAccessException;
+    Collection<IntergateTransaction> find(@Param("status") String status, @Param("carrierName") String carrierName, @Param("count") int count) throws DataAccessException;
 
 }
